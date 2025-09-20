eef04d9ce0cc7cf1be9086210c46eda9fb5b630d
maboni <martin@maboni.dk>
2025-09-20 12:25:16 +0200

    🔧 Add Vitest type reference to Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+/// <reference types="vitest" />
 
 // https://vitejs.dev/config/
 export default defineConfig({
